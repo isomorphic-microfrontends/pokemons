e9dcef6ded78828bdc2808acb4dd1f9dd8dd7111
Tarun Kumar Sukhu <tksukhu@gmail.com>
2020-11-08 11:23:36 +0530

    possible fix for ssr handling of api calls

--- src/api.js
+++ src/api.js
@@ -1,16 +1,50 @@
-import fetch from "unfetch";
+import fetch from "isomorphic-unfetch";
+import https from "https";
+
+const nodeGetPokemons = () => {
+  return new Promise((resolve, reject) => {
+    https
+      .get("https://pokeapi.co/api/v2/pokemon?limit=12", function(res) {
+        // A chunk of data has been recieved.
+        let data = "";
+        res.on("data", chunk => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        res.on("end", () => {
+          resolve(JSON.parse(data).results);
+        });
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+};
 
 export async function getPokemons() {
   try {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
-    const { results } = await res.json();
-    const pokemons = results.map((pokeman, index) => {
-      const paddedId = ("00" + (index + 1)).slice(-3);
+    if (typeof window === "undefined") {
+      const results = await nodeGetPokemons();
+      const pokemons = results.map((pokeman, index) => {
+        const paddedId = ("00" + (index + 1)).slice(-3);
 
-      const img = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
-      return { ...pokeman, img };
-    });
-    return pokemons;
+        const img = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+        return { ...pokeman, img };
+      });
+
+      return pokemons;
+    } else {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+      const { results } = await res.json();
+      const pokemons = results.map((pokeman, index) => {
+        const paddedId = ("00" + (index + 1)).slice(-3);
+
+        const img = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+        return { ...pokeman, img };
+      });
+      return pokemons;
+    }
   } catch (err) {
     console.error(err);
   }
